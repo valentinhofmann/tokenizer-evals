3bc4a0a5f75d9bacd538533a8f71b17d7100ce6b
Tyler Murray <tylerm@allenai.org>
2025-04-11 13:22:39 -0700

    Adds more datasets

--- src/analyze_tokenizer.py
+++ src/analyze_tokenizer.py
@@ -1,47 +1,61 @@
 import argparse
+import logging
 
 from transformers import AutoTokenizer
 
 import utils
 
+# Set up logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--tokenizer", type=str, required=True)
+    parser.add_argument("--n_samples", type=int, required=False, default=1000)
     parser.add_argument(
-        "--tokenizer",
-        type=str,
-        required=True
-    )
-    parser.add_argument(
-        "--n_samples",
-        type=int,
-        required=False,
-        default=1000
-    )
-    parser.add_argument(
-        "--english_only", 
+        "--english_only",
         action="store_true",
         required=False,
+    )
+    parser.add_argument("--seed", type=int, required=False, default=42)
+    parser.add_argument("--dataset", type=str, required=False, default=None)
+    parser.add_argument(
+        "--write_file",
+        action="store_true",
+        required=False,
+        help="Write the results to a file per dataset",
     )
     args = parser.parse_args()
 
     # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
 
-    for dataset_name in utils.CHAT_DATASETS:
-
-        # Load dataset
-        data = utils.load_data(
-            dataset_name, 
-            args.n_samples, 
-            args.english_only
+    if args.dataset:
+        logger.info(f" Evaluating {args.tokenizer} on dataset: {args.dataset}")
+        process_dataset(
+            args.dataset, tokenizer, args.n_samples, args.english_only, args.write_file
         )
+    else:
+        logger.info(
+            f" Evaluating {args.tokenizer} on all datasets with up to {args.n_samples} samples"
+        )
+        for name in utils.ALL_DATASETS:
+            process_dataset(
+                name, tokenizer, args.n_samples, args.english_only, args.write_file
+            )
 
-        # Compute metrics
-        data = utils.compute_metrics(data, tokenizer)
 
-        # Display metrics
-        utils.display_metric(dataset_name, data, "fertility")
+def process_dataset(dataset, tokenizer, n_samples, english_only, write_file=False):
+    data = utils.load_data(dataset, n_samples, english_only)
+    result = utils.compute_metrics(data, tokenizer)
+    metric = "fertility"
+
+    if write_file:
+        utils.write_json(dataset, result, metric, tokenizer)
+
+    utils.display_metric(dataset, result, metric)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,47 +1,144 @@
+import json
+
 from datasets import load_dataset
 
-CHAT_DATASETS = {
-    "wildchat", 
-    "chatbot_arena"
+SEED = 123
+CHAT_DATASETS = {"wildchat", "chatbot_arena"}
+CODE_DATASETS = {"mbpp", "human_eval"}
+CORE_DATASETS = {"mmlu", "arc_challenge", "gsm8k"}
+
+
+DATSET_TO_SPLIT = {
+    "wildchat": "train",
+    "chatbot_arena": "train",
+    "mbpp": "test",
+    "human_eval": "test",
+    "mmlu": "auxiliary_train",
+    "arc_challenge": "train",
+    "gsm8k": "train",
 }
+
+DATSET_TO_SUBSET = {
+    "mmlu": "all",
+    "gsm8k": "main",
+    "arc_challenge": "ARC-Challenge",
+}
+
+ALL_DATASETS = CHAT_DATASETS | CODE_DATASETS | CORE_DATASETS
 
 FULL_NAMES = {
+    "mbpp": "Muennighoff/mbpp",
+    "human_eval": "openai/openai_humaneval",
     "wildchat": "allenai/WildChat",
-    "chatbot_arena": "lmsys/chatbot_arena_conversations"
+    "chatbot_arena": "lmsys/chatbot_arena_conversations",
+    "mmlu": "cais/mmlu",
+    "arc_challenge": "allenai/ai2_arc",
+    "gsm8k": "openai/gsm8k",
 }
 
-CONVERSATION_COLS = {
-    "wildchat": "conversation",
-    "chatbot_arena": "conversation_a"
+CODE_COLUMNS = {
+    "mbpp": ["text", "code"],
+    "human_eval": ["prompt", "canonical_solution"],
 }
+
+CORE_COLUMNS = {
+    "mmlu": ["question", "choices"],
+    "arc_challenge": ["question", "choices"],
+    "gsm8k": ["question", "answer"],
+}
+
+CONVERSATION_COLS = {"wildchat": "conversation", "chatbot_arena": "conversation_a"}
 
 
 def load_data(dataset_name, n_samples=1000, english_only=True):
     if dataset_name in CHAT_DATASETS:
         return load_chat_data(dataset_name, n_samples, english_only)
+    elif dataset_name in CODE_DATASETS:
+        return load_code_data(dataset_name, n_samples)
+    elif dataset_name in CORE_DATASETS:
+        return load_core_data(dataset_name, n_samples)
     else:
         raise NotImplementedError(f"Dataset '{dataset_name}' is not supported yet.")
 
 
+def load_core_data(dataset_name, n_samples=1000):
+    core_data = load_dataset(
+        FULL_NAMES[dataset_name],
+        DATSET_TO_SUBSET[dataset_name],
+        split=DATSET_TO_SPLIT[dataset_name],
+    )
+
+    core_data_sample = (
+        core_data.shuffle(seed=SEED)
+        .select(range(min(n_samples * 3, len(core_data))))
+        .to_pandas()
+    )
+
+    core_data_sample["text"] = core_data_sample.apply(
+        lambda x: "\n".join(
+            [
+                "\n".join(x[col]) if col == "choices" else "\n".join(x[col])
+                for col in CORE_COLUMNS[dataset_name]
+            ]
+        ),
+        axis=1,
+    )
+
+    core_data_sample = core_data_sample[core_data_sample["text"].str.strip() != ""]
+
+    return core_data_sample.sample(min(n_samples, len(core_data)), random_state=SEED)
+
+
+def load_code_data(dataset_name, n_samples=1000):
+    code_data = load_dataset(
+        FULL_NAMES[dataset_name], split=DATSET_TO_SPLIT[dataset_name]
+    )
+
+    code_data_sample = (
+        code_data.shuffle(seed=SEED)
+        .select(range(min(n_samples * 3, len(code_data))))
+        .to_pandas()
+    )
+
+    code_data_sample["text"] = code_data_sample.apply(
+        lambda x: "\n".join([x[col] for col in CODE_COLUMNS[dataset_name]]),
+        axis=1,
+    )
+
+    code_data_sample = code_data_sample[code_data_sample["text"].str.strip() != ""]
+
+    return code_data_sample.sample(min(n_samples, len(code_data)), random_state=SEED)
+
+
 def load_chat_data(dataset_name, n_samples=1000, english_only=True):
-    chat_data = load_dataset(FULL_NAMES[dataset_name], split="train")
+    chat_data = load_dataset(
+        FULL_NAMES[dataset_name], split=DATSET_TO_SPLIT[dataset_name]
+    )
 
     # Pre-sample larger chunk
-    chat_data_sample = chat_data.shuffle(seed=123).select(range(n_samples * 3)).to_pandas()
+    chat_data_sample = (
+        chat_data.shuffle(seed=SEED).select(range(n_samples * 3)).to_pandas()
+    )
 
     # Process user input message
-    chat_data_sample["user_input"] = chat_data_sample[CONVERSATION_COLS[dataset_name]].apply(lambda x: x[0]["content"])
-    chat_data_sample = chat_data_sample[chat_data_sample["user_input"].apply(lambda x: x.strip() != "")]
+    chat_data_sample["text"] = chat_data_sample[CONVERSATION_COLS[dataset_name]].apply(
+        lambda x: x[0]["content"]
+    )
+    chat_data_sample = chat_data_sample[
+        chat_data_sample["text"].apply(lambda x: x.strip() != "")
+    ]
 
     # Optional: filter non-English messages
     if english_only:
-        chat_data_sample = chat_data_sample[chat_data_sample["language"].str.lower() == "english"]
+        chat_data_sample = chat_data_sample[
+            chat_data_sample["language"].str.lower() == "english"
+        ]
 
     # Final sampling
-    return chat_data_sample.sample(n_samples, random_state=123)
+    return chat_data_sample.sample(n_samples, random_state=SEED)
 
 
-def compute_metrics(data, tokenizer, text_col="user_input"):
+def compute_metrics(data, tokenizer, text_col="text"):
 
     # Split into words and tokens
     data["tokens"] = data[text_col].apply(lambda x: tokenizer.tokenize(x))
@@ -53,10 +150,34 @@ def compute_metrics(data, tokenizer, text_col="user_input"):
 
     # Compute fertility
     data["fertility"] = data.apply(
-        lambda r: r["n_tokens"] / r["n_words"] if r["n_words"] > 0 else 0, 
-        axis=1
+        lambda r: r["n_tokens"] / r["n_words"] if r["n_words"] > 0 else 0, axis=1
     )
     return data
+
+
+def write_json(dataset_name, data, metric, tokenizer, output_dir=".output"):
+    import os
+    import re
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    if metric not in data.columns:
+        raise ValueError(f"Metric '{metric}' not found in dataset.")
+
+    values = data[metric]
+    out = {}
+    safe_tokenizer_name = re.sub(r"[^\w\-_.]", "_", tokenizer.name_or_path)
+
+    with open(
+        f"{output_dir}/{safe_tokenizer_name}_{dataset_name}_{metric}.json", "w"
+    ) as f:
+        out["mean"] = values.mean()
+        out["std"] = values.std()
+        out["min"] = values.min()
+        out["max"] = values.max()
+        out["25pct"] = values.quantile(0.25)
+        out["median"] = values.median()
+        f.write(json.dumps(out, indent=4))
 
 
 def display_metric(dataset_name, data, metric):
